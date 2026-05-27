b4acf5d50fcd430cb164cca8b2cf9cfba7103956
Kovinskiy Danil <danyakovinskiy2000@mail.ru>
2026-05-27 17:43:34 +0300

    fix : images

--- app/invitation/components/invitation-body.tsx
+++ app/invitation/components/invitation-body.tsx
@@ -290,7 +290,7 @@ export const InvitationBody = ({
             <WeddingCalendar />
             <div className="love-couple">
               <img src="/images/egor.png" alt="Саша" />
-              <img className='w-50!' src="/images/veronika3.png" alt="Катя" />
+              <img className='' src="/images/veronika3.png" alt="Катя" />
             </div>
             <div className="love-nameplate">
               <span>{COUPLE_TITLE}</span>
